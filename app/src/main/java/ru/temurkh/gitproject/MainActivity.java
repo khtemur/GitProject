9051a0cbdd468f7ba5347c97621e0f70658c7738
temurkh <khabelashvilitemur@gmail.com>
2022-01-10 10:26:38 +0300

    Initial commit

---
+++ app/src/main/java/ru/temurkh/gitproject/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.temurkh.gitproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
